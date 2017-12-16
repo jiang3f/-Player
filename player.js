0f6d7b4f45ed5c76a358fc72eecf88e8c12f9b8b
jiang3f <jiang3f@gmail.com>
2017-12-16 13:23:03 -0800

    Add message handlers

--- player.js
+++ player.js
@@ -3,32 +3,100 @@ var Rubiks = Rubiks || {};
 
 Rubiks.Player = function(element) {
 
-  cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
-    
-  this.element_ = element;
+    cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
 
-  // video element
-  this.mediaElement_ = this.element_.querySelector('video');
+    /*
+    *    class members
+    */
+    // 
+    this.element = element;
 
-  this.mediaManager_ = new window.cast.receiver.MediaManager(this.mediaElement_);
-  
-  // cast receier manager
-  this.receiverManager_ = window.cast.receiver.CastReceiverManager.getInstance();
-  
-  // config
-  this.appConfig_ = new cast.receiver.CastReceiverManager.Config();
-  
+    // video element
+    this.mediaElement = this.element.querySelector('video');
+
+    // media manager
+    this.mediaManager = new window.cast.receiver.MediaManager(this.mediaElement);
+
+    // cast receier manager
+    this.receiverManager = window.cast.receiver.CastReceiverManager.getInstance();
+
+    // config
+    this.appConfig = new cast.receiver.CastReceiverManager.Config();
+
+   
+    /*====================================================================================
+    *    For media applications, override/provide any event listeners on the MediaManager. 
+    *===================================================================================*/
+
+    /**
+     * Called when we receive a LOAD message. Calls load().
+     * @param {cast.receiver.MediaManager.Event} event The load event.
+     * @private
+     */
+    /*
+    this.mediaManager.onLoad = (function() {
+        this.mediaManager.origOnLoad = this.mediaManager.onLoad;
+        return function(event) {
+            console.log("onload");
+            this.mediaManager.origOnLoad(event);
+        }
+    }());
+
+    this.mediaManager['origOnLoad'] = this.mediaManager.onLoad;
+    this.mediaManager.onLoad = function (event) {
+        console.log("onLoad");
+        this.mediaManager['origOnLoad'](event);
+    }
+
+    this.mediaManager['origOnPlay'] = this.mediaManager.onPlay;
+    this.mediaManager.onPlay = function (event) {
+        //do whatever is needed for the receiver application logic
+        console.log("onPlay");
+        this.mediaManager['origOnPlay'](event);
+    }
+    */
+
+    /**
+     * The original metadata error callback.
+     * @private {?function(!cast.receiver.MediaManager.LoadInfo)}
+     */
+    this.onLoadOrig = this.mediaManager.onLoad.bind(this.mediaManager);
+    this.mediaManager.onLoad = this.onLoad.bind(this);
+
 }
+
+
+// ========================================================================================================================================
+// ========================================================================================================================================
 
 /**
  * set app config.
  */
 Rubiks.Player.prototype.set_appConfig = function() {
-    this.appConfig_.statusText = 'Ready to play';
-    this.appConfig_.maxInactivity = 6000;
+    this.appConfig.statusText = 'Ready to play';
+
+    // 100 minutes for testing, use default 10sec in prod by not setting this value
+    console.log("set maxInactivity to 100 Mns for testing!");
+    this.appConfig.maxInactivity = 6000;
     
 }
-    
+
+/**
+ * Called when the media could not be successfully loaded. Transitions to
+ * IDLE state and calls the original media manager implementation.
+ *
+ * @see cast.receiver.MediaManager#onLoadMetadataError
+ * @param {!cast.receiver.MediaManager.LoadInfo} event The data
+ *     associated with a LOAD event.
+ * @private
+ */
+Rubiks.Player.prototype.onLoad = function(event) {
+    console.log('onLoad');
+    this.onLoadOrig(event);
+};
+  
+  
+
 /**
  * Starts the player.
  */
@@ -36,5 +104,6 @@ Rubiks.Player.prototype.start = function() {
 
     this.set_appConfig();
 
-    this.receiverManager_.start(this.appConfig);
+    this.receiverManager.start(this.appConfig);
+
 };
