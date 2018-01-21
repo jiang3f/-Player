49fc23b797b127ae931eada4d1d041509a5341e9
jiang3f <jiang3f@gmail.com>
2018-01-21 01:17:12 -0800

    enable subtitle stream.

--- player.js
+++ player.js
@@ -1,7 +1,8 @@
 /* name space */
 var Dash = Dash || {};
 
-Dash.Player = function(element) {
+Dash.Player = function(element) 
+{
 
     cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
     cast.player.api.setLoggerLevel(cast.player.api.LoggerLevel.DEBUG);
@@ -26,6 +27,15 @@ Dash.Player = function(element) {
 
     // player
     this.player = null;
+
+    // protocol
+    this.protocol = null;
+
+  /**
+   * Text Tracks currently supported.
+   */
+  this.textTrackType = null;
+  
    
     /*====================================================================================
     *    For media applications, override/provide any event listeners on the MediaManager. 
@@ -38,6 +48,12 @@ Dash.Player = function(element) {
     this.onLoadOrig = this.mediaManager.onLoad.bind(this.mediaManager);
     this.mediaManager.onLoad = this.onLoad.bind(this);
 
+  /**
+   * The original metadataLoaded callback
+   * @private {?function(!cast.receiver.MediaManager.LoadInfo)}
+   */
+   this.onMetadataLoadedOrig = this.mediaManager.onMetadataLoaded.bind(this.mediaManager);
+   this.mediaManager.onMetadataLoaded = this.onMetadataLoaded.bind(this);
 }
 
 
@@ -47,7 +63,8 @@ Dash.Player = function(element) {
 /**
  * set app config.
  */
-Dash.Player.prototype.set_appConfig = function() {
+Dash.Player.prototype.set_appConfig = function() 
+{
     this.appConfig.statusText = 'Ready to play';
 
     // 100 minutes for testing, use default 10sec in prod by not setting this value
@@ -59,7 +76,8 @@ Dash.Player.prototype.set_appConfig = function() {
 /**
  * Called when we receive a LOAD message. Calls load().
  */
-Dash.Player.prototype.onLoad = function(event) {
+Dash.Player.prototype.onLoad = function(event) 
+{
     console.log('onLoad');
 
     if (this.player !== null) {
@@ -71,18 +89,31 @@ Dash.Player.prototype.onLoad = function(event) {
         console.log('Starting media application');
         var url = event.data['media']['contentId'];
 
-        window.host = new cast.player.api.Host({'mediaElement':this.mediaElement, 'url':url});
+        window.host = new cast.player.api.Host({'mediaElement':this.mediaElement, 'url':url,useRelativeCueTimestamps: false});
+
+        /*
+         *If your server requires CORS and cookie information in order to access the media, set the property withCredentials to true and set the header information as needed.
+         *
+        window.host.updateSegmentRequestInfo = function(requestInfo) {
+            // example of setting CORS withCredentials
+            requestInfo.withCredentials = true;
+            // example of setting headers
+            requestInfo.headers = {};
+            requestInfo.headers['content-type'] = 'text/xml;charset=utf-8';
+          };
+        */
+
         var ext = url.substring(url.lastIndexOf('.'), url.length);
         var initStart = event.data['media']['currentTime'] || 0;
         var autoplay = event.data['autoplay'] || true;
-        var protocol = null;
         this.mediaElement.autoplay = autoplay;  // Make sure autoplay get's set
+
         if (url.lastIndexOf('.m3u8') >= 0) {
-            protocol = cast.player.api.CreateHlsStreamingProtocol(host);
+            this.protocol = cast.player.api.CreateHlsStreamingProtocol(host);
         } else if (url.lastIndexOf('.mpd') >= 0) {
-            protocol = cast.player.api.CreateDashStreamingProtocol(host);
+            this.protocol = cast.player.api.CreateDashStreamingProtocol(host);
         } else if (url.indexOf('.ismc') >= 0) {
-            protocol = cast.player.api.CreateSmoothStreamingProtocol(host);
+            this.protocol = cast.player.api.CreateSmoothStreamingProtocol(host);
         }
 
         host.onError = function(errorCode) {
@@ -94,10 +125,11 @@ Dash.Player.prototype.onLoad = function(event) {
         };
 
         console.log("we have protocol " + ext);
-        if (protocol !== null) {
+        if (this.protocol !== null) {
             console.log("Starting Media Player Library");
             this.player = new cast.player.api.Player(host);
-            this.player.load(protocol, initStart);
+            this.player.load(this.protocol, initStart);
+
         }
         else {
             this.onLoadOrig(event);
@@ -105,7 +137,87 @@ Dash.Player.prototype.onLoad = function(event) {
     }
 };
   
+/**
+ * Called when metadata is loaded
+ * @private
+ */
+Dash.Player.prototype.onMetadataLoaded = function(info) 
+{
+  console.log('onMetadataLoaded');
 
+  this.readSideLoadedTextTrackType(info);
+
+  var streamCount = this.protocol.getStreamCount();
+
+  var streamInfo;
+  for (var i = 0; i < streamCount; i++) {
+      streamInfo = this.protocol.getStreamInfo(i);
+      this.protocol.enableStream(i, true);
+  }
+
+  this.player.enableCaptions(true);            
+  
+  /*
+   * for debugging
+   */
+  var bufferDuration = this.player.getMaxBufferDuration();
+
+};
+  
+
+
+/**
+ * 
+ */
+Dash.Player.prototype.readSideLoadedTextTrackType =
+function(info) 
+{
+    if (!info.message || !info.message.media || !info.message.media.tracks) 
+    {
+        return;
+    }
+
+    for (var i = 0; i < info.message.media.tracks.length; i++) 
+    {
+        var oldTextTrackType = this.textTrackType_;
+        if (info.message.media.tracks[i].type != cast.receiver.media.TrackType.TEXT) 
+        {
+            continue;
+        }
+
+        var trackContentId = info.message.media.tracks[i].trackContentId;
+        var trackContentType = info.message.media.tracks[i].trackContentType;
+        
+        if ((trackContentId && this.getExtension(trackContentId) == 'vtt') | (trackContentType && trackContentType.indexOf('text/vtt') === 0)) 
+        {
+            console.log("found vtt");
+            this.textTrackType = 'vtt';
+        }
+        else 
+        {
+            console.log("unsupported text track");
+        }
+    }
+};
+
+/**
+ */
+Dash.Player.getExtension = function(url) {
+    var parts = url.split('.');
+    // Handle files with no extensions and hidden files with no extension
+    if (parts.length === 1 || (parts[0] === '' && parts.length === 2)) {
+      return '';
+    }
+    return parts.pop().toLowerCase();
+  };
+  
+  
+/**
+ */
+Dash.Player.prototype.onLoadSuccess = function() {
+    console.log("onLoadSuccess")
+};
+    
 /**
  * Starts the player.
  */
